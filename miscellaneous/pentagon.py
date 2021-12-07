519bdc73b24f4bcf767e3a39910fa6b4afabee98
leenakhalili <engleena@yahoo.com>
2021-12-07 08:50:13 +0300

    Five sided pentagon

---
+++ miscellaneous/pentagon.py
@@ -0,0 +1,7 @@
+# Write code to draw a regular pentagon (a five-sided figure with all sides the same length).
+import turtle
+ 
+t = turtle.Turtle()
+for i in range(5):
+   t.forward(100) #Assuming the side of a pentagon is 100 units
+   t.right(72) #Turning the turtle by 72 degree
